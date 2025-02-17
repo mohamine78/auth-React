25cc80c43bdeff62cbad1b3da14c7a8f955e552e
mohamine78 <mohamine78200@outlook.fr>
2025-02-17 13:45:18 +0100

    forme sign up a jour

--- social-front/src/pages/HomePage.jsx
+++ social-front/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Bienvenue sur la page d'accueil</h1>
+      <h1>Bienvenue sur la page d'accueil !</h1>
     </div>
   );
 };
